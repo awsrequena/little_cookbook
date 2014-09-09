bec9f66ed295173bc59cc1c61fcf3dac054943e3
Christian Requena <requena@amazon.de>
2014-09-09 11:43:29 +0200

    add stupid file test - for berkshelf cache build verification

--- little_cookbook/recipes/default.rb
+++ little_cookbook/recipes/default.rb
@@ -1,3 +1,10 @@
 execute "Write text file /etc/little_cookbook" do
   command "echo #{node[:little][:thing]} > /etc/little_cookbook"
 end
+
+cookbook_file "/tmp/guagua.txt" do
+  source "guagua.txt"
+  mode 0755
+  owner "root"
+  group "root"
+end
